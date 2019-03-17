ce6db92300e006ebfe3d33c872946084923ef95a
voruti <unconfigured>
2019-03-17 10:27:40 +0100

    creating file

--- src/typ21/Starter.java
+++ src/typ21/Starter.java
@@ -1,10 +1,12 @@
 /**
- * 
+ * default package
  */
 package typ21;
 
+import java.io.File;
+
 /**
- * @author Till
+ * @author voruti
  *
  */
 public class Starter {
@@ -13,8 +15,14 @@ public class Starter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		if (args.length > 0) {
+			String workingDirectory = System.getProperty("user.dir");
+
+			File file = new File(workingDirectory, args[0]);
+			System.out.println(file.getAbsolutePath());
+
+		}
 	}
 
 }
